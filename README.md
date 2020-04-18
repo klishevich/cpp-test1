4b3ba335becf3bef162026d41fae4d0e7c60dab6
Michael Klishevich <busation@gmail.com>
2020-04-18 21:55:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning C++
+
+## How to start
+
+1. Edit. If needed edit helloworld.cpp file
+2. Build. Terminal -> Run build task...
+3. Run. Debug -> Run without debugging.
+# cpp-test1
---
+++ src/helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << std::endl;
+
+    int myVar = 10;
+    std::string myString = "myVar: " + std::to_string(myVar);
+    std::cout << myString << std::endl;
+
+    int *ptr = &myVar;
+    *ptr = 20;
+    std::string myString2 = "myVar: " + std::to_string(myVar);
+    std::cout << myString2 << std::endl;
+
+    int **ptr2 = &ptr;
+    **ptr2 = 30;
+    std::string myString3 = "myVar: " + std::to_string(myVar);
+    std::cout << myString3 << std::endl;
+
+    return 0;
+}
